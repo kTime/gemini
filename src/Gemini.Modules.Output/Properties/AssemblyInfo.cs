0e639bf23968adce4c21e5bc4e0b617a84655740
danjmackay <danjmackay@gmail.com>
2015-06-24 14:46:06 +1000

    0.6.0-ci6039 Customised Gemini.nuspec for kTime fork. Modified UpdateFloatingWindows in ShellView.xaml to set floating window Title based on LayoutAnchorable or LayoutDocument Title

--- src/Gemini.Modules.Output/Properties/AssemblyInfo.cs
+++ src/Gemini.Modules.Output/Properties/AssemblyInfo.cs
@@ -24,4 +24,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.6.0.0")]
 [assembly: AssemblyFileVersion("0.6.0.0")]
-[assembly: AssemblyInformationalVersion("0.6.0-ci6038")]
+[assembly: AssemblyInformationalVersion("0.6.0-ci6039")]
--- src/Gemini/Modules/Shell/Views/ShellView.xaml.cs
+++ src/Gemini/Modules/Shell/Views/ShellView.xaml.cs
@@ -40,6 +40,30 @@ namespace Gemini.Modules.Shell.Views
 	        {
                 window.Icon = mainWindowIcon;
 	            window.ShowInTaskbar = showFloatingWindowsInTaskbar;
+
+                //Set the window title based on the SinglePane Anchorable content or the RootDocument
+                //depending on the type of floating window
+                var floatAnchWindow = window.Model as Xceed.Wpf.AvalonDock.Layout.LayoutAnchorableFloatingWindow;
+
+                if (floatAnchWindow != null)
+                {
+                    var lAnch = ((Xceed.Wpf.AvalonDock.Layout.LayoutAnchorablePane)floatAnchWindow.SinglePane);
+                    if (lAnch != null)
+                    {
+                        var selContent = ((Xceed.Wpf.AvalonDock.Layout.LayoutAnchorable)lAnch.SelectedContent);
+                        window.Title = selContent.Title; 
+                    }
+                }
+                else
+                {
+                    //It's a Document floating window
+                    var floatDocWindow = window.Model as Xceed.Wpf.AvalonDock.Layout.LayoutDocumentFloatingWindow;
+                    var lAnch = ((Xceed.Wpf.AvalonDock.Layout.LayoutDocument)floatDocWindow.RootDocument);
+                    if (lAnch != null)
+                    {
+                        window.Title = lAnch.Title; 
+                    }
+                }                
 	        }
 	    }
 	}
