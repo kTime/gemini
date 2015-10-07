4f8cf18f950942751a3338d3c8a860c5ce3ff5ab
kTime <kTime@users.noreply.github.com>
2015-10-07 14:14:43 +1000

    Updated assembly version for Output (0.6.0-ci6100) and Inspector modules (0.6.0-ci6039)

--- src/Gemini.Modules.Inspector/Properties/AssemblyInfo.cs
+++ src/Gemini.Modules.Inspector/Properties/AssemblyInfo.cs
@@ -24,9 +24,9 @@ using System.Windows.Markup;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5185b64e-2aa5-4fab-b1d3-e48e111baa92")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
+[assembly: AssemblyInformationalVersion("0.6.0-ci6039")]
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: XmlnsDefinition("http://schemas.timjones.tw/gemini", "Gemini.Modules.Inspector.Controls")]
